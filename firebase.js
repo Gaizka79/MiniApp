7ad06f7595221fdbf049f023f2e0cd0b300e8d4c
Gaizka79 <agaizka79@gmail.com>
2022-04-28 11:53:09 +0200

    commit final

--- firebase.js
+++ firebase.js
@@ -1,9 +1,10 @@
 const admin = require('firebase-admin');
-//const fbProyectId = process.env.FB_PROJECT_ID;
+const fbProyectId = process.env.FB_PROJECT_ID;
 
 // Initialize firebase admin SDK
+/*
 admin.initializeApp({
-  credential: admin.credential.cert(process.env),
+  credential: admin.credential.cert(fbProyectId),
   storageBucket: FB_PROJECT_ID.appspot.com
 })
 
@@ -12,4 +13,4 @@ const bucket = admin.storage().bucket()
 
 module.exports = {
   bucket
-}
+}*/
